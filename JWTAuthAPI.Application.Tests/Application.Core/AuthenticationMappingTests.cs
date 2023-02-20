02099ff0a95009b711148ea29476b08d6c7765e8
Angel Dhungana <angeldhungana32@gmail.com>
2023-02-20 12:38:07 -0700

    example tests

---
+++ JWTAuthAPI.Application.Tests/Application.Core/AuthenticationMappingTests.cs
@@ -0,0 +1,42 @@
+﻿using JWTAuthAPI.Application.Core.DTOs.Authentication;
+using JWTAuthAPI.Application.Core.DTOs.UserAccount;
+using JWTAuthAPI.Application.Core.Entities.Identity;
+using JWTAuthAPI.Application.Core.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthAPI.Application.Tests.Application.Core
+{
+    public class AuthenticationMappingTests
+    {
+        [Fact]
+        public void ShouldMapApplicationUserToAuthenticateResponseDTO()
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                Email = "test@test.com",
+                Id = Guid.NewGuid()
+            };
+            var result = user.ToResponseDTO("token");
+            Assert.NotNull(result);
+            Assert.True(result.GetType().IsAssignableFrom(typeof(AuthenticateResponse)));
+            Assert.NotNull(result.Id);
+            Assert.NotNull(result.Email);
+            Assert.NotNull(result.FirstName);
+            Assert.NotNull(result.LastName);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionWhenMappingNullToResponseDTO()
+        {
+            ApplicationUser? nullUser = default;
+            Assert.Throws<ArgumentNullException>(() => 
+                AuthenticationMappings.ToResponseDTO(nullUser, "token"));
+        }
+    }
+}
